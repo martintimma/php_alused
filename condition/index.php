58a7baaef1f42bf2914226ae3a8228bf2631f912
martin.timma <martin.timma@khk.ee>
2018-12-13 16:21:01 +0200

    tingimuslause tsükkel

--- condition/index.php
+++ condition/index.php
@@ -1,19 +1,21 @@
 <?php
 // tingimuslaused
-// if(tingimus {
-// tegevused, mis toimuvad, kui tingimus kehtib
-// } else {
-// tegevused, mis toimuvad, kui tingimus ei kehti
-// }
 
-echo "<h4>Tingimuslaused</h4>";
+echo '<h4>Tingimuslaused</h4>';
 
-$arv = rand(0, 100); //juhuslik arv 0st 100ni
+$foor = 'kollane';
 
-if($arv % 2 == 0) {
-    //antud arv on paaris ja ta on roheline
-    echo "<p style='color: green'>".$arv."</p>";
-} else {
-    // arv paaritu ja punane
-    echo "<p style='color: red'>".$arv."</p>";
+switch ($foor) {
+    case 'punane':
+        echo '<p style="color: red">punane!</p>';
+        break;
+    case 'kollane':
+        echo '<p style="color: yellow">kollane!</p>';
+    break;
+    case 'roheline':
+        echo '<p style="color: green">roheline!</p>';
+    break;
+    default:
+        echo '<p>mingi teine</p>';
+        break;
 }
