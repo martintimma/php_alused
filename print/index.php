04230cfbd99d33d9b6123c4ed746d91c500c3b70
martin.timma <martin.timma@khk.ee>
2018-12-13 15:04:42 +0200

    teksti väljastus

---
+++ print/index.php
@@ -0,0 +1,7 @@
+<?php
+//teksti väljastus print käsuga
+print "See on esimene lause php keeles"."<br>";
+print "See on teine lause php keeles"."<br>";
+//teksti väljastus  echo käsuga
+echo "see tekst on echo abil väljastatud"."<br>";
+?>
