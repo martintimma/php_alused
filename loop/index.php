c877f7c4ef2cd8e1d9c9a54835f88ae142425dad
martin.timma <martin.timma@khk.ee>
2018-12-14 10:52:10 +0200

    tsükkel ja arvutustabel

--- loop/index.php
+++ loop/index.php
@@ -24,3 +24,31 @@ for($rida = 1; $rida <= 5; $rida++){
     echo '</tr>';
 }
 echo '</table>';
+
+echo "<hr>";
+// korrutustabel
+echo '<table style="border-collapse: collapse">';
+// ülemine punane riba - algus - tühi kast
+echo '<th style="width: 20px; text-align: center; border: solid 1px black; background: lightcoral">&nbsp;</th>';
+// ülemine punane riba - numbrid 1-10
+for($arv2 = 1; $arv2 <= 10; $arv2++){
+    echo '<th style="width: 20px; text-align: center; border: solid 1px black; background: lightcoral">';
+    echo $arv2;
+    echo '</th>';
+}
+// põhitabel
+for($arv1 = 1; $arv1 <= 10; $arv1++){
+    echo '<tr>'; // rea algus
+    // üks punane kast
+    echo '<th style="width: 20px; text-align: center; border: solid 1px black; background: lightcoral">';
+    echo $arv1;
+    echo '</th>';
+    // korrutustabeli elemendid - korrutamise tulemused
+    for($arv2 = 1; $arv2 <= 10; $arv2++) {
+        echo '<td style="width: 20px; text-align: center; border: solid 1px black;">';
+        echo $arv1 * $arv2;
+        echo '</td>';
+    }
+    echo '</tr>'; // rea lõpp
+}
+echo '</table>'; // tabeli lõpp
