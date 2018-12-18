e19b5d83c1f86781ef864527287dcd995a886d00
martin.timma <martin.timma@khk.ee>
2018-12-18 12:17:48 +0200

    massiivid - väljastamine ja elementide töötlus

--- arrays/index.php
+++ arrays/index.php
@@ -3,10 +3,20 @@ $kasutajad = array(); // tühi massiiv
 $kasutajad[] = 'alice';
 $kasutajad[] = 'bob';
 array_push($kasutajad, 'lucy');
-
 $lisakasutajad = array('mark','george');
-echo '<pre>';
-print_r($kasutajad);
-print_r($lisakasutajad);
-echo '</pre>';
-//var_dump($kasutajad);
+
+//massiivi pikkus
+$kasutajadkokku = count($kasutajad);
+for($i = 0; $i < 3; $i++) {
+    echo $kasutajad[$i].'<br>';
+}
+array_shift($kasutajad);
+for($i = 0; $i < 3; $i++) {
+    echo $kasutajad[$i] . '<br>';
+}
+
+$uuedkasutajad = array_merge($kasutajad, $lisakasutajad);
+for($i = 0; $i < $uuedkasutajad; $i++) {
+    echo $uuedkasutajad[$i] . '<br>';
+}
+
