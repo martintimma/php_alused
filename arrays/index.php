430a4621f9a5748dc1aa8b344b0018787e8e5310
martin.timma <martin.timma@khk.ee>
2018-12-18 12:40:21 +0200

    massiivid - for, foreach, if

--- arrays/index.php
+++ arrays/index.php
@@ -1,28 +1,26 @@
 <?php
-$kasutajad = array(); // tühi massiiv
-$kasutajad[] = 'alice';
-$kasutajad[] = 'bob';
-array_push($kasutajad, 'lucy');
-$lisakasutajad = array('mark','george');
-
-//massiivi pikkus
-$kasutajadkokku = count($kasutajad);
-for($i = 0; $i < 3; $i++) {
-    echo $kasutajad[$i].'<br>';
+$kasutajad = array(
+    array(
+        'alice',
+        'Alice',
+        'Wonderland',
+        'female'
+    ),
+    array(
+        'bob',
+        'Bob',
+        'Builder',
+        'male'
+    )
+);
+for ($i = 0; $i < count($kasutajad); $i++ ){
+    foreach($kasutajad[$i] as $info) {
+        if ($kasutajad[$i][3] == 'female') {
+            echo '<div style="color: red">';
+        } else {
+            echo '<div style="color: blue">';
+        }
+        echo $info . '</div>';
+    }
+    echo '<hr>';
 }
-array_shift($kasutajad);
-for($i = 0; $i < 3; $i++) {
-    echo $kasutajad[$i] . '<br>';
-}
-
-$uuedkasutajad = array_merge($kasutajad, $lisakasutajad);
-for($i = 0; $i < count($uuedkasutajad); $i++) {
-    echo $uuedkasutajad[$i] . '<br>';
-}
-
-echo '<hr>';
-
-foreach ($uuedkasutajad as $kasutaja) {
-    echo $kasutaja.'<br>';
-}
-
